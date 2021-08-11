4faf66a83690fac93fac89faa91edcb0c28f06bd
velvetxq <temptapetemp0tapetemp550@yandex.ru>
2021-08-12 02:22:51 +0300

    /

--- src/requester.py
+++ src/requester.py
@@ -11,12 +11,12 @@ class Requester:
                           'Chrome/92.0.4476.0 Safari/537.36'
 
     def get_request(self, page_num):
-        url = f'https://vkfaces.com/vk/users/{randint(1,5)}00/1/{page_num}'
+        url = f'https://vkfaces.com/vk/users/{randint(1,600)}/{randint(1,9)}/{page_num}'
         return requests.get(url, headers={'User-Agent': self.user_agent}).text
 
     def starting_processing(self, pages_count):
         with Pool(cpu_count()) as pool:
             self.results_of_requests = \
-                pool.map(self.get_request,
-                         range(1, round(pages_count / 10) + 1 if pages_count > 5 else pages_count + 2))
+                pool.map(self.get_request, range(int(pages_count / 100) + 1))
         return self.results_of_requests
+
